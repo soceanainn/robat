452672999f9bd49d16b46c7600b72c539ff1e9f0
Seamus O Ceanainn <seamus.oceanainn@zalando.ie>
2021-03-25 15:11:17 +0000

    Add basic files

---
+++ app.js
@@ -0,0 +1,15 @@
+const Discord = require('discord.js');
+require('dotenv').config();
+
+const client = new Discord.Client();
+
+client.on('ready', () => {
+  console.log('Bot is ready');
+});
+
+client.login(process.env.BOT_TOKEN)
+
+client.on('message', (msg) => {
+  if (msg.content === '!tearma') msg.reply('Hi');
+});
+
