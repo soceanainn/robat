9117714e76c2a9c1a43249ef1ee2d83c59b7dd24
Seamus O Ceanainn <seamus.oceanainn@zalando.ie>
2021-03-27 14:50:07 +0000

    WIP

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const Discord = require('discord.js');
 require('dotenv').config();
 const Teasaras = require('./commands/teasaras');
+const Focloir = require('./commands/focloir');
+
 const client = new Discord.Client();
 
 client.on('ready', () => {
@@ -10,12 +12,20 @@ client.on('ready', () => {
 client.login(process.env.BOT_TOKEN);
 
 client.on('message', (msg) => {
-  message = msg.content.trim();
-  if (!message.startsWith('!') || msg.author.bot) return;
+  try {
+    message = msg.content.trim();
+    if (!message.startsWith('!') || msg.author.bot) return;
 
-  if (message === 'Róbat test')
-    return msg.reply('Hi'); // Test command
+    if (message === 'Róbat test')
+      return msg.reply('Hi'); // Test command
 
-  if (message.startsWith(Teasaras.prefix))
-    return Teasaras.handle(msg);
+    if (message.startsWith(Teasaras.prefix))
+      return Teasaras.handle(msg);
+
+    if (message.startsWith(Focloir.prefix))
+      return Focloir.handle(msg);
+
+  } catch (error){
+    console.error('Unhandled exception: "' + error + '" for message: "' + msg.content + '"');
+  }
 });
---
+++ commands/focloir.js
@@ -0,0 +1,41 @@
+const Discord = require('discord.js');
+const jsdom = require("jsdom");
+const { JSDOM } = jsdom;
+const https = require('https');
+
+const prefix = '!foclóir';
+module.exports.prefix = prefix;
+module.exports.handle = handle;
+
+function handle(message){
+    let searchTerm = message.content.trim().substring(prefix.length).trim();
+    try {
+        https.get('https://www.teanglann.ie/ga/fb/' + encodeURI(searchTerm),(res) => {
+            let data = "";
+
+            res.on('data', (d) => {
+                data += d
+            });
+
+            res.on('end', () => {
+                let parsed = new JSDOM(data.toString());
+                let entry = parsed.window.document.querySelector(".fb.entry");
+                if (!entry)
+                    return message.reply('níor aimsíodh "' + searchTerm + '" sa foclóir');
+
+                const embed = new Discord.MessageEmbed()
+                    .setTitle(searchTerm.charAt(0).toUpperCase() + searchTerm.slice(1))
+                    .setURL('https://www.teanglann.ie/ga/fb/' + encodeURI(searchTerm));
+
+                embed.setDescription(entry.querySelector(".fb.pos").childNodes[0].title);
+                embed.addField('Brí:', entry.querySelector(".fb.definition.clickable").textContent);
+
+                message.channel.send(embed);
+            });
+
+        });
+    } catch(error) {
+        console.error("");
+        message.reply('bhí fadhb nascadh le teanglann.ie. Bain triail eile as arís níos déanaí');
+    }
+}
